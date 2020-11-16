4220d5a9058d0e7f64159e37b5b5822b84849407
Aviael <73803725+Aviael@users.noreply.github.com>
2020-11-15 17:01:43 -0700

    Update Weather Psuedocode.py
    redone weather api

--- Weather Psuedocode.py
+++ Weather Psuedocode.py
@@ -1,25 +1,29 @@
-print ('Welcome. Please enter your zip code or city.')
-zipCity = input ('In the field below, please type your City or Zip Code:')
+import json
+import requests
+#python is installing the requests library
+def this_api():
+    """welcome user and explain"""
+    print("Welcome to my attempt at an API. Below you will be prompted for your City or Zip code to get the data from OpenWeatherMap.org.")
+    
+this_api()
+#function
+while True:
+    zipCity = input ('In the field below, please type your City or Zip Code:')
 #this is the request field
 
-$ pip install requests-2.24.0.tar.gz
-#python is installing the requests library
+api_key = "0d10eedd637db9ce12d3b7ecc52964d8"
 
-response = requests.get('http://google.com')
-enter(zipCity)
-if response !== "Search instead for...":
-	response = requests.get('http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={0d10eedd637db9ce12d3b7ecc52964d8}')
-#this is where python is accessing the api
-		if response == 200
-  			response.put (zipCity)
-  		else:
-  			print(f"(There was an issue accessing the website. Please wait a few minutes and try again")
-  			response = requests.get('http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={0d10eedd637db9ce12d3b7ecc52964d8}')
-  #this is checking to make sure the api wwas able to be pulled succesfully
- else:
- 	print(This is not a valid city and/or zip code. Please try again.)
+city = ('zipCity')
+url = "https://api.openweathermap.org/data/2.5/onecall?city=%s&appid=%s&units=imperial"% (city, api_key)
 
-weatherPattern = x
-  #at this point, the website should have displayed what the weather pattern is
-print("The weather for, 'zipCity' is 'weatherPattern.' ")
+response=requests.get(url)
+data = json.loads(response.text)
+current = data["current"]["dt"]
+if response = '200'
+    print(current)
+    else:
+        print(f"(There was an issue accessing the website. Please wait a few minutes and try again.")
+ #this is checking to make sure the api was able to be pulled succesfully
+
+print("The weather for, 'zipCity' is 'current.' ")
 #the final step is the weather forecast being displayed for the User.
