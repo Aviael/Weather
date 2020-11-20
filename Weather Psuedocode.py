71a47f701e2bf686f108ee158773e2a9d76a7e68
Aviael <73803725+Aviael@users.noreply.github.com>
2020-11-20 01:17:23 -0700

    Update Weather Psuedocode.py
    Final Changes

--- Weather Psuedocode.py
+++ Weather Psuedocode.py
@@ -3,9 +3,10 @@ import requests
 #python is installing the requests library
 def this_api():
     """welcome user and explain"""
-    print("Welcome to my attempt at an API. Below you will be prompted for your City or Zip code to get the data from OpenWeatherMap.org.")
+    print("Welcome to my attempt at using an API from openweathermap.com. Below you will be prompted for your City or Zip code to get the data from OpenWeatherMap.org.")
     
 this_api()
+
 #function
 while True:
     zipCity = input ('In the field below, please type your City or Zip Code:')
@@ -16,13 +17,13 @@ api_key = "0d10eedd637db9ce12d3b7ecc52964d8"
 city = ('zipCity')
 url = "https://api.openweathermap.org/data/2.5/onecall?city=%s&appid=%s&units=imperial"% (city, api_key)
 
-response=requests.get(url)
+response = requests.get(url)
 data = json.loads(response.text)
 current = data["current"]["dt"]
-if response = '200'
+if response == 'True':
     print(current)
-    else:
-        print(f"(There was an issue accessing the website. Please wait a few minutes and try again.")
+else:
+    print(f"(There was an issue accessing the website. Please wait a few minutes and try again.")
  #this is checking to make sure the api was able to be pulled succesfully
 
 print("The weather for, 'zipCity' is 'current.' ")
